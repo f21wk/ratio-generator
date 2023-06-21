a361ff478c3334c1a2ab0c844fca050307c7a732
f21wk <107116872+f21wk@users.noreply.github.com>
2023-06-21 22:04:07 +0900

    🎉 最低限のHTMLを追加

---
+++ js/main.js
@@ -0,0 +1,6 @@
+(() =>{
+    const [$WIDHT,$HEIGHT] = document.querySelectorAll('.size input');
+    const [silver,gold,half] = document.querySelectorAll('input[name="ratio"]');
+    const [w,h,wh] = document.querySelectorAll('input[name="base-size"]');
+
+})()
